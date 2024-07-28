03b4114187d7cefeb3848c545a04610ae1c20879
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-07-28 17:03:53 +0530

    Add files via upload

---
+++ src/app/Components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='flex mt-[40vh] animate-spin mx-auto justify-center'>
+         <div className= 'w-12 h-12 rounded-full border-4 border-t-black'></div>
+
+    </div>
+
+  )
+}
+
+export default Loading
